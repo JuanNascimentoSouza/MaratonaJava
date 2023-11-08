fedc1d061b6d9b0ea4dc4f16751208e07c1d9aba
Juan <104234658+JuanNascimentoSouza@users.noreply.github.com>
2023-11-07 23:50:23 -0300

    TiposPrimitivos,Operadores,EstruturasCondicionais,Arrays

---
+++ MaratonaJava/src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        //int double float char byte short long boolean
+        int age = 18;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        long numeroGrande = 100000;
+        char caractere = 65;
+        char caractere2 = '\u0041';
+        String nome = "Juan";
+
+        System.out.println("A idade é " + age + " anos!");
+        System.out.println(verdadeiro);
+        System.out.println(caractere);
+        System.out.println(caractere2);
+    }
+}
---
+++ MaratonaJava/src/academy/devdojo/maratonajava/introducao/Aula06EstruturaDeRepeticao02.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula06EstruturaDeRepeticao02 {
+    public static void main(String[] args) {
+        for(int i = 1; i <=10000; i++){
+            System.out.println(i);
+        }
+    }
+}
