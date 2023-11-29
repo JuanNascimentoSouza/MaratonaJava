998b122431423837152ed2408c5a8b65fe60cf54
Juan <104234658+JuanNascimentoSouza@users.noreply.github.com>
2023-11-28 22:59:53 -0300

    JDBC, mysql conn

---
+++ MaratonaJava/src/JDBC/test/ConnectionFactoryTest.java
@@ -0,0 +1,9 @@
+package JDBC.test;
+
+import JDBC.conn.ConnectionFactory;
+
+public class ConnectionFactoryTest {
+    public static void main(String[] args) {
+        ConnectionFactory.getConnection();
+    }
+}
